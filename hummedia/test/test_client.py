82a6c6330b732e7f420016a5109f5c3db2829ede
Ian Hunter <beanland2@gmail.com>
2015-01-15 14:59:56 -0700

    Test showing that IC client does not return correct .icf keys.

--- hummedia/test/test_client.py
+++ hummedia/test/test_client.py
@@ -1,11 +1,8 @@
+from zipfile import ZipFile
+from StringIO import StringIO
 import json
 
-def test_download_ic_file(app, ACCOUNTS):
-  from zipfile import ZipFile
-  from StringIO import StringIO
-
-  app.login(ACCOUNTS['SUPERUSER'])
-
+def create_video_in_collection(app):
   v = app.post('/video')
   data = json.loads(v.data)
   vid_pid = data['pid']
@@ -18,6 +15,26 @@ def test_download_ic_file(app, ACCOUNTS):
   membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
   membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
   assert membership_result.status_code is 200
+
+  return vid_pid, col_pid
+
+def test_ic_file_correct_icf_format(app, ACCOUNTS, ASSETS):
+  app.login(ACCOUNTS['SUPERUSER'])
+  vid_pid, col_pid = create_video_in_collection(app)
+
+  annotation_result = app.get('/annotation?client=ic&collection=' + col_pid + '&dc:relation=' + vid_pid)
+  assert annotation_result.headers.get('Content-Type') == 'application/zip'
+
+  z = ZipFile(StringIO(annotation_result.data))
+  items = z.namelist()
+
+  contents = z.read(filter(lambda fname: fname.endswith('.icf'), items)[0])
+  contents = json.loads(contents);
+  assert {'video', 'annotation', 'subtitle'} <= set(contents)
+
+def test_download_ic_file(app, ACCOUNTS):
+  app.login(ACCOUNTS['SUPERUSER'])
+  vid_pid, col_pid = create_video_in_collection(app)
 
   # now make a collection-based annotation
   collection_based = {"media":[{"id":vid_pid,"name":"Media0","url":["https://milo.byu.edu///movies/50aba99cbe3e2dadd67872da44b0da94/54131f93/0033467.mp4","https://milo.byu.edu///movies/b4861e89ca5c8adf5ae37281743206cd/54131f93/0033467.webm"],"target":"hum-video","duration":300.011,"popcornOptions":{"frameAnimation":True},"controls":False,"tracks":[{"name":"Layer 0","id":"0","trackEvents":[{"id":"TrackEvent0","type":"skip","popcornOptions":{"start":0,"end":5,"target":"target-0","__humrequired":False,"id":"TrackEvent0"},"track":"0","name":"TrackEvent0"}]}],"clipData":{}}]}
@@ -52,19 +69,7 @@ def test_download_ic_file_subs_only(app, ACCOUNTS, ASSETS):
   from StringIO import StringIO
 
   app.login(ACCOUNTS['SUPERUSER'])
-
-  v = app.post('/video')
-  data = json.loads(v.data)
-  vid_pid = data['pid']
-
-  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
-  data = json.loads(c.data)
-  col_pid = data['pid']
-
-  # attach video to collection
-  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
-  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
-  assert membership_result.status_code is 200
+  vid_pid, col_pid = create_video_in_collection(app)
 
   with open(ASSETS + 'subs.vtt') as f:
     data = {
