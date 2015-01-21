cf027e1d9393a60c693b39a8c375ca63068d5b76
Ian Hunter <beanland2@gmail.com>
2015-01-21 15:17:07 -0700

    Test showing that read/write groups for annotations are broken.

--- hummedia/test/test_annotate.py
+++ hummedia/test/test_annotate.py
@@ -20,7 +20,7 @@ def test_required_annotation_audio(app, ACCOUNTS, ASSETS):
                       headers={'Content-Type': 'application/json'})
   assert response.status_code == 200
 
-def test_collection_write_access_can_annotate(app, ACCOUNTS):
+def test_collection_write_access_faculty_can_modify(app, ACCOUNTS):
   ''' If we create a collection as a superuser, annotate it, and then
       give someone write access to the collection it belongs to,
       then that person should have write access to the annotations as well. '''
@@ -53,7 +53,7 @@ def test_collection_write_access_can_annotate(app, ACCOUNTS):
   col_based_id = col_data['media'][0]['tracks'][0]['id']
 
   # now grant write access to the faculty
-  patch = {"dc:rights": {"read": [ACCOUNTS['FACULTY']], "write": [ACCOUNTS['FACULTY']]}}
+  patch = {"dc:rights": {"read": [ACCOUNTS['FACULTY']['username']], "write": [ACCOUNTS['FACULTY']['username']]}}
   result = app.patch('/collection/' + col_pid, data=json.dumps(patch), headers={'Content-Type': 'application/json'})
   assert result.status_code is 200
 
@@ -63,6 +63,43 @@ def test_collection_write_access_can_annotate(app, ACCOUNTS):
 
   assert col_based_patch.status_code is 200, "Faculty with write access could not modify collection-based annotations. Status: %d" % col_based_patch.status_code
   assert req_patch.status_code is 200, "Faculty with write access could not modify required annotations. Status: %d" % req_patch.status_code
+
+def test_collection_write_access_ta_can_annotate(app, ACCOUNTS):
+  ''' If we create a collection as a superuser and then
+      give a student write access to the collection it belongs to,
+      then that person should have write access to the annotations as well. '''
+
+  app.login(ACCOUNTS['SUPERUSER'])
+  
+  v = app.post('/video')
+  data = json.loads(v.data)
+  vid_pid = data['pid']
+
+  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  data = json.loads(c.data)
+  col_pid = data['pid']
+
+  # attach video to collection
+  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
+  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
+  assert membership_result.status_code is 200
+
+  # now grant write access to the TA
+  patch = {"dc:rights": {"read": [ACCOUNTS['STUDENT']['username']], "write": [ACCOUNTS['STUDENT']['username']]}}
+  result = app.patch('/collection/' + col_pid, data=json.dumps(patch), headers={'Content-Type': 'application/json'})
+  assert result.status_code is 200
+  
+  app.login(ACCOUNTS['STUDENT'])
+
+  collection_based = {"media":[{"id":vid_pid,"name":"Media0","url":["https://milo.byu.edu///movies/50aba99cbe3e2dadd67872da44b0da94/54131f93/0033467.mp4","https://milo.byu.edu///movies/b4861e89ca5c8adf5ae37281743206cd/54131f93/0033467.webm"],"target":"hum-video","duration":300.011,"popcornOptions":{"frameAnimation":True},"controls":False,"tracks":[{"name":"Layer 0","id":"0","trackEvents":[{"id":"TrackEvent0","type":"skip","popcornOptions":{"start":0,"end":5,"target":"target-0","__humrequired":False,"id":"TrackEvent0"},"track":"0","name":"TrackEvent0"}]}],"clipData":{}}]}
+  col_result = app.post('/annotation?client=popcorn&collection=' + col_pid, data=json.dumps(collection_based), headers={'Content-Type':'application/json'})
+ 
+  assert col_result.status_code is 200, "TA could not create collection-based annotation"
+  col_data = json.loads(col_result.data)
+  col_based_id = col_data['media'][0]['tracks'][0]['id']
+  col_based_patch = app.patch('/annotation/' + col_based_id + '?client=popcorn', data=json.dumps(collection_based), headers={'Content-Type': 'application/json'})
+
+  assert col_based_patch.status_code is 200, "TA with write access could not modify collection-based annotations. Status: %d" % col_based_patch.status_code
 
 def test_collection_annotations_not_with_required(app, ACCOUNTS):
   ''' There's a bug where collection-based annotations are coming back with
