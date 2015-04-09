5c379d082ef25a1848aa79330b7e9f5f8c6b2632
Ian Hunter <beanland2@gmail.com>
2015-04-09 13:41:41 -0600

    Another test to ensure collection membership working correctly.

--- hummedia/test/test_collection.py
+++ hummedia/test/test_collection.py
@@ -79,3 +79,24 @@ def test_student_no_write_access_of_collection_cannot_add_videos(app, ACCOUNTS):
   new_c = app.get('/collection/' + col_pid + '?full=true')
   result = json.loads(new_c.data)
   assert len(result['videos']) is 0
+
+def test_write_access_of_collection_can_add_someone_elses_videos(app, ACCOUNTS):
+  app.login(ACCOUNTS['SUPERUSER'])
+  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  v = app.post('/video', data=json.dumps({'dc:creator': ACCOUNTS['STUDENT']['username']}), headers={'Content-Type': 'application/json'})
+  
+  c_data = json.loads(c.data)
+  new = {'dc:creator': ACCOUNTS['FACULTY']['username']}
+  col_pid = c_data['pid']
+  r = app.patch('/collection/' + col_pid, data=json.dumps(new), headers={'Content-Type': 'application/json'})
+
+  app.login(ACCOUNTS['FACULTY'])
+  vid_data =  json.loads(v.data)
+  vid_pid = vid_data['pid']
+  
+  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
+  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
+  assert membership_result.status_code is 200
+
+  new_c = app.get('/collection/' + col_pid + '?full=true')
+  assert len(json.loads(new_c.data)['videos']) is 1
