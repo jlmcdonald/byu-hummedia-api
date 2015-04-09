3faaecfba2aa07ab995d3d9e9a9511c114e02be7
Ian Hunter <beanland2@gmail.com>
2015-04-09 13:08:20 -0600

    Showcasing bug where a student can add any video to any collection.

--- hummedia/test/test_collection.py
+++ hummedia/test/test_collection.py
@@ -35,3 +35,46 @@ def test_owner_of_collection_can_add_videos(app, ACCOUNTS):
 
   new_c = app.get('/collection/' + col_pid + '?full=true')
   assert len(json.loads(new_c.data)['videos']) is 1
+
+def test_write_access_of_collection_can_add_videos(app, ACCOUNTS):
+  app.login(ACCOUNTS['SUPERUSER'])
+  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  v = app.post('/video', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+
+  c_data = json.loads(c.data)
+  user = ACCOUNTS['STUDENT']['username']
+  patch_data = {'dc:rights': {'read': [user], 'write': [user]}}
+  col_pid = c_data['pid']
+  r = app.patch('/collection/' + col_pid, data=json.dumps(patch_data), headers={'Content-Type': 'application/json'})
+  
+  app.login(ACCOUNTS['STUDENT'])
+  vid_data =  json.loads(v.data)
+  vid_pid = vid_data['pid']
+  
+  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
+  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
+  assert membership_result.status_code is 200
+
+  new_c = app.get('/collection/' + col_pid + '?full=true')
+  result = json.loads(new_c.data)
+  assert len(result['videos']) is 1
+
+def test_student_no_write_access_of_collection_cannot_add_videos(app, ACCOUNTS):
+  app.login(ACCOUNTS['SUPERUSER'])
+  c = app.post('/collection', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  v = app.post('/video', data=json.dumps({}), headers={'Content-Type': 'application/json'})
+  
+  app.login(ACCOUNTS['STUDENT'])
+  vid_data =  json.loads(v.data)
+  vid_pid = vid_data['pid']
+  c_data = json.loads(c.data)
+  col_pid = c_data['pid']
+  
+  membership = [{"collection":{"id":col_pid,"title":"Something"},"videos":[vid_pid]}]
+  membership_result = app.post('/batch/video/membership', data=json.dumps(membership), headers={'Content-Type': "application/json"})
+  assert membership_result.status_code is 401
+
+  app.login(ACCOUNTS['SUPERUSER'])
+  new_c = app.get('/collection/' + col_pid + '?full=true')
+  result = json.loads(new_c.data)
+  assert len(result['videos']) is 0
